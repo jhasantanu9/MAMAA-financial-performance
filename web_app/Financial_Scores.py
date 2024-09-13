b8665ab80c249b887ffd8c51c97a5fd9c999be4b
Santanu Jha <145570201+jhasantanu9@users.noreply.github.com>
2024-09-14 03:44:06 +0530

    Update Financial_Scores.py

--- web_app/Financial_Scores.py
+++ web_app/Financial_Scores.py
@@ -5,9 +5,9 @@ import plotly.graph_objs as go
 import plotly.express as px
 import streamlit.components.v1 as components
 from sklearn.preprocessing import MinMaxScaler
-import tensorflow as tf
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 from sklearn.metrics import mean_absolute_error, mean_squared_error
+import os
 
 @st.cache_data
 def create_dataset(data, time_step=60):
@@ -163,7 +163,11 @@ def main(merged_df, final_data):
         yaxis_title='Closing Price')         
     st.plotly_chart(fig)
 
-    model = load_model(f'./{selected_company}_lstm_model.h5')
+    # Define the path to the current directory where app.py and models are stored
+    path = os.path.dirname(os.path.abspath(__file__))
+
+    # Load the selected company's model using the path
+    model = load_model(os.path.join(path, f'{selected_company}_lstm_model.h5'))
 
     scaler = MinMaxScaler(feature_range=(0, 1))
     X_scaled = scaler.fit_transform(final_data[final_data['symbol'] == selected_company][['close']])
